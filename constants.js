3330a1008428e31035e71c015a6301da28e213b3
Caitlin <cjcroteau@gmail.com>
2022-04-21 22:50:06 +0000

    update constants for movement and messages

--- constants.js
+++ constants.js
@@ -1,7 +1,41 @@
 const IP = 'localhost';
 const PORT = 50541;
 
+const EXIT_KEY = '\u0003';
+const MOVE_UP_KEY = "w";
+const MOVE_LEFT_KEY = "a";
+const MOVE_DOWN_KEY = "s";
+const MOVE_RIGHT_KEY = "d";
+const EXIT = process.exit();
+const UP = "Move: up";
+const LEFT = "Move: left";
+const DOWN = "Move: down";
+const RIGHT = "Move: right";
+
+const MESSAGES = {
+  h: "Hello..",
+  g: "Goodbye",
+  i: "I win!",
+  u: "How are you?",
+  o: "I am ok!",
+  1: "100%!!!!",
+  "?": "WHAT???",
+  "*": "hssssss..."
+};
+
+
 module.exports = {
   IP,
   PORT,
+  EXIT_KEY,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  EXIT,
+  UP,
+  LEFT,
+  DOWN,
+  RIGHT,
+  MESSAGES
 };
--- input.js
+++ input.js
@@ -1,7 +1,11 @@
 
 // setup interface to handle user input from stdin
+const { EXIT_KEY, MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, EXIT, UP, 
+  LEFT, DOWN, RIGHT, MESSAGES } = require('./constants');
 
 let connection;
+let lastCommand = false;
+let messages = MESSAGES;
 
 const setupInput = function(conn) {
   connection = conn;
@@ -9,60 +13,93 @@ const setupInput = function(conn) {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
+  
   stdin.on("data", handleUserInput);
+
+  setInterval(() => {
+    if (lastCommand) connection.write(lastCommand);
+  }, 500);
+
   return stdin;
 };
 
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
+  if (key === EXIT_KEY) {
+    EXIT;
   }
 
-  if (key === 'w') {
-    connection.write('Move: up');
+  if (key === MOVE_UP_KEY) {
+    lastCommand = UP;
   }
   
-  if (key === 'a') {
-    connection.write("Move: left");
+  if (key === MOVE_LEFT_KEY) {
+    lastCommand = LEFT;
   }
 
-  if (key === 's') {
-    connection.write('Move: down');
+  if (key === MOVE_DOWN_KEY) {
+    lastCommand = DOWN;
   }
 
-  if (key === 'd') {
-    connection.write('Move: right');
+  if (key === MOVE_RIGHT_KEY) {
+    lastCommand = RIGHT;
   }
 
-  if (key === 'h') {
-    connection.write('Say: Hello');
-  }
-
-  if (key === 'b') {
-    connection.write('Say: Goodbye');
-  }
-
-  if (key === 'i') {
-    connection.write('Say: I win!');
-  }
-
-  if (key === 'u') {
-    connection.write('Say: How are you!');
-  }
-
-  if (key === 'o') {
-    connection.write('Say: I am ok!');
-  }
-
-  if (key === '1') {
-    connection.write('Say: 100%!!!!');
-  }
-
-  if (key === '?') {
-    connection.write('Say: WHAT???');
+  if (key in messages) {
+    connection.write(messages[newkey])
   }
 };
+
+
+
+// const handleUserInput = function(key) {
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+
+//   if (key === 'w') {
+//     lastCommand = 'Move: up';
+//   }
+  
+//   if (key === 'a') {
+//     lastCommand = "Move: left";
+//   }
+
+//   if (key === 's') {
+//     lastCommand = 'Move: down';
+//   }
+
+//   if (key === 'd') {
+//     lastCommand = 'Move: right';
+//   }
+
+//   if (key === 'h') {
+//     connection.write('Say: Hello');
+//   }
+
+//   if (key === 'b') {
+//     connection.write('Say: Goodbye');
+//   }
+
+//   if (key === 'i') {
+//     connection.write('Say: I win!');
+//   }
+
+//   if (key === 'u') {
+//     connection.write('Say: How are you!');
+//   }
+
+//   if (key === 'o') {
+//     connection.write('Say: I am ok!');
+//   }
+
+//   if (key === '1') {
+//     connection.write('Say: 100%!!!!');
+//   }
+
+//   if (key === '?') {
+//     connection.write('Say: WHAT???');
+//   }
+// };
 
 module.exports = {
   setupInput
